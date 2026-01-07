492d101c3e6069151cdd005809879b89b6565abf
FixedDolphin927 <fixdol1127@gmail.com>
2026-01-07 14:38:44 +0900

    Fixed LICENCE and README

--- src/main/java/com/fxd927/createcubechemistry/CreateCubeChemistry.java
+++ src/main/java/com/fxd927/createcubechemistry/CreateCubeChemistry.java
@@ -1,32 +1,18 @@
 package com.fxd927.createcubechemistry;
 
+import com.fxd927.createcubechemistry.entry.CCCItems;
 import com.mojang.logging.LogUtils;
+//import com.simibubi.create.foundation.data.CreateRegistrate;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 @Mod(CreateCubeChemistry.MODID)
@@ -34,16 +20,20 @@ public class CreateCubeChemistry
 {
     public static final String MODID = "createcubechemistry";
     private static final Logger LOGGER = LogUtils.getLogger();
+    //public static final CreateRegistrate REGISTRATE = CreateRegistrate.create("createcubechemistry");
 
     public CreateCubeChemistry()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
+        registerEntries(modEventBus);
+        modEventBus.register(this);
         modEventBus.addListener(this::commonSetup);
-
         MinecraftForge.EVENT_BUS.register(this);
+    }
 
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    private void registerEntries(IEventBus modEventBus) {
+        CCCItems.register();
+        //REGISTRATE.registerEventListeners(modEventBus);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
---
+++ src/main/java/com/fxd927/createcubechemistry/entry/CCCItems.java
@@ -0,0 +1,22 @@
+package com.fxd927.createcubechemistry.entry;
+
+//import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.world.item.Item;
+
+//import static com.fxd927.createcubechemistry.CreateCubeChemistry.REGISTRATE;
+
+public class CCCItems {
+    //public static final ItemEntry<Item> CHEMICAL_CUBE;
+    //public static final ItemEntry<Item> ELECTROLYSIS_CUBE;
+
+    static {
+        //CHEMICAL_CUBE = REGISTRATE.item("chemical_cube", Item::new)
+        //.properties(prop -> prop.stacksTo(64))
+        //.register();
+        //ELECTROLYSIS_CUBE = REGISTRATE.item("electrolysis_cube", Item::new)
+        //.properties(prop -> prop.stacksTo(64))
+        //.register();
+    }
+
+    public static void register() {}
+}
